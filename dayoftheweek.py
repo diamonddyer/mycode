3382bd73e1bf2a9b6c44233892d9fe7f83f65951
diamonddyer <desi.l.dyer@gmail.com>
2023-11-06 18:17:49 +0000

    day of the week challange

---
+++ dayoftheweek.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+user_name = input("What is your name? ")
+day_of_the_week = input("What day of the week is it? ")
+
+
+print(f"Hello, {user_name}! Happy {day_of_the_week}!")
+
+
+
